cb126d2c4e9f9a3c1df35b82c4689e9a17bd508f
Gulzhasm <gulzhasm@gmail.com>
2024-01-07 18:51:08 +0600

    add pytest

---
+++ test/test_circle.py
@@ -0,0 +1,20 @@
+import math
+
+import pytest
+
+import src.shapes as shapes
+
+
+class TestCircle:
+
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+
+    def test_area(self):
+        assert True
+
+    def test_two(self):
+        assert True
